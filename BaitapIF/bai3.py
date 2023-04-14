79c3d604b55af481cad86620fa99f74436769b27
vohoangtruonghuy <128212110+vohoangtruonghuy@users.noreply.github.com>
2023-04-14 15:56:44 +0700

    Update bai3.py

--- BaitapIF/bai3.py
+++ BaitapIF/bai3.py
@@ -1,5 +1,13 @@
-n=int(input('Nhập số năm: '))
-if(n % 4):
-    print('Năm không nhuận')
+t=int(input('Nhập tháng: '))
+while not (1<= t and t <=12):
+    t=int(input('Nhập từ tháng 1 đến tháng 12'))
+if t in (4,6,9,11):
+        print('tháng', t ,'có 30 ngày')
+elif t==2:
+        n=int(input('Nhập năm: '))
+        if n%4==0:
+         print('tháng',t,'năm',n,'có 29 ngày')
+        else:
+         print('tháng',t,'năm',n,'có 28 ngày')
 else:
-    print('Năm nhuận')
+    print('tháng',t,'có 31 ngày')
