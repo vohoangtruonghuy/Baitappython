f3b76943fb519a29b5b1813b631def7785bf5abe
vohoangtruonghuy <128212110+vohoangtruonghuy@users.noreply.github.com>
2023-04-21 16:11:11 +0700

    Create bai1.py

---
+++ baitap_for.py/bai1.py
@@ -0,0 +1,6 @@
+n=int(input('Nhập một số n thỏa mãn điều kiện 0<n<100: '))
+for i in range(1):
+    if n>0 and n<100:
+     print('Thỏa mãn điều kiện')
+else:
+     print('Không thỏa mãn điều kiện vui lòng nhập lại')
